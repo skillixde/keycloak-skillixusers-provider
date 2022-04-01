e6bfb498c2cdcc0f9e4848d5b6221b49ac79b3c0
Sangamithra-ApiDigit <100684844+Sangamithra-ApiDigit@users.noreply.github.com>
2022-04-01 19:28:35 +0200

    Add files via upload
    Initial commit

---
+++ KeycloakCustomProvidersApplication.java
@@ -0,0 +1,13 @@
+package de.skillix.auth.provider.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakCustomProvidersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakCustomProvidersApplication.class, args);
+	}
+
+}
---
+++ KeycloakCustomProvidersApplicationTests.java
@@ -0,0 +1,13 @@
+package de.skillix.auth.provider.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakCustomProvidersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
