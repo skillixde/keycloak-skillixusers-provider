efaeb22f8d5a84fbd8ba3d7b2be9addeb688b487
Achille Nana Chimi <me@nanachimi.com>
2022-04-20 17:05:56 +0200

    timestamp datatype changed to string

--- src/main/java/de/skillix/keycloak/spi/userprovider/UserCounter.java
+++ src/main/java/de/skillix/keycloak/spi/userprovider/UserCounter.java
@@ -4,12 +4,10 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.ZonedDateTime;
-
 @Data
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserCounter {
     private int totalCount;
-    private ZonedDateTime timestamp;
+    private String timestamp;
 }
